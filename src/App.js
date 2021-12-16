d821b1ab66097051aae7e33f1bd5f5a86f12bc50
ckaynatividad <natividad.ckay@gmail.com>
2021-12-15 17:07:41 -0800

    supabase key added, routes added to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import BookList from './views/Books/BookList';
 import './App.css';
+import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <BrowserRouter>
+        <NavLink to="/books" data-testid="book-link">
+      Books
+        </NavLink>
+        <Switch>
+          <Route path="/books" exact>
+            <h1>Booklist</h1>
+            <BookList />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
